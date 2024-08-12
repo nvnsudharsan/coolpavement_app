b3865be4b474b40986726831b87b64b5d9a2f60b
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2024-08-12 11:06:30 -0500

    Update coolpavement.py

--- coolpavement.py
+++ coolpavement.py
@@ -37,7 +37,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 # Function to find and concatenate Excel files
 def find_and_concat_excel_files(folder_path):
     try:
@@ -217,25 +216,19 @@ fig2.update_layout(
     showgrid=False)
 )
 
-# Add sunrise and sunset times to the plots with white for day and grey for night
+# Add sunrise and sunset times to the plots
 daily_profile = locations_avg['control_temperature']
 date_list = np.unique(daily_profile.index.strftime('%Y-%m-%d'))
 for i, date in enumerate(date_list):
     sunrise_time, sunset_time = get_sun_rise_set_time(date)
-    
-    # White for daytime
-    fig2.add_vrect(x0=sunrise_time, x1=sunset_time, fillcolor="#FFFFFF", opacity=0.3, layer="below", line_width=0)
-    
-    # Grey for nighttime before sunrise
+    fig2.add_vrect(x0=sunrise_time, x1=sunset_time, fillcolor="#EF810E", opacity=0.25, layer="below", line_width=0)
     if i == 0:
-        fig2.add_vrect(x0=daily_profile.index[0], x1=sunrise_time, fillcolor="#808080", opacity=0.3, layer="below", line_width=0)
-    
-    # Grey for nighttime after sunset
+        fig2.add_vrect(x0=daily_profile.index[0], x1=sunrise_time, fillcolor="#053752", opacity=0.25, layer="below", line_width=0)
     if i != len(date_list) - 1:
         next_sunrise_time, _ = get_sun_rise_set_time(date_list[i+1])
-        fig2.add_vrect(x0=sunset_time, x1=next_sunrise_time, fillcolor="#808080", opacity=0.3, layer="below", line_width=0)
+        fig2.add_vrect(x0=sunset_time, x1=next_sunrise_time, fillcolor="#053752", opacity=0.25, layer="below", line_width=0)
     else:
-        fig2.add_vrect(x0=sunset_time, x1=daily_profile.index[-1], fillcolor="#808080", opacity=0.3, layer="below", line_width=0)
+        fig2.add_vrect(x0=sunset_time, x1=daily_profile.index[-1], fillcolor="#053752", opacity=0.25, layer="below", line_width=0)
 
 # Display plots in Streamlit app
 st.subheader("Comparison of Air Temperatures recorded in Treatment area and Reference")
